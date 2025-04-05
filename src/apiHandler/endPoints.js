222554009dd15b19d64c1d3e95cf7014b79ffe37
Divyanka <divyankak36@gmail.com>
2025-04-05 11:00:17 +0530

    updating security vulnerabilities

--- src/apiHandler/endPoints.js
+++ src/apiHandler/endPoints.js
@@ -1,15 +1,15 @@
-export const BASE_URL="https://exercisedb.p.rapidapi.com";
+export const BASE_URL = "http://exercisedb.p.rapidapi.com";
 
-export const ENDPOINTS={
-    "allExercises":"/exercises",
-    "bodyPartList":"/exercises/bodyPartList",
-    "bodyPart":"/exercises/bodyPart/<bodyPart>",
-    "targetMuscleList":"/exercises/targetList",
-    "targetMuscle":"/exercises/target/<muscleName>",
-    "equipmentList":"/exercises/equipmentList",
-    "equipment":"/exercises/equipment/<equipmentType>",
-    "searchByName":"/exercises/name/<exerciseName>"
-}
+export const ENDPOINTS = {
+  allExercises: "/exercises",
+  bodyPartList: "/exercises/bodyPartList",
+  bodyPart: "/exercises/bodyPart/<bodyPart>",
+  targetMuscleList: "/exercises/targetList",
+  targetMuscle: "/exercises/target/<muscleName>",
+  equipmentList: "/exercises/equipmentList",
+  equipment: "/exercises/equipment/<equipmentType>",
+  searchByName: "/exercises/name/<exerciseName>",
+};
 
-export const API_KEY='33d2b5030cmsh0d116296f80875cp18e488jsn7440110ed0ef';
-export const API_HOST='exercisedb.p.rapidapi.com'
+export const API_KEY = "33d2b5030cmsh0d116296f80875cp18e488jsn7440110ed0ef";
+export const API_HOST = "exercisedb.p.rapidapi.com";
